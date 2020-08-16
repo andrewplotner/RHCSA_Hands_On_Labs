2da1ddd5eddb4dc022897ae354d82b75621433b6
andrewplotner <andrewplotner@yahoo.com>
2020-08-16 13:19:13 -0400

    Create step1-foreground.sh

---
+++ 23. Exercise- Firewalld/step1-foreground.sh
@@ -0,0 +1,5 @@
+echo "This website was made in /root" > /root/index.html
+mv /root/index.html /var/www/html/index.html
+touch /var/www/html/SELinux-defaults.html
+systemctl restart httpd
+systemctl start httpd
