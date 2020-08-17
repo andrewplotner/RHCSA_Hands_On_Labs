6a9f55f7dc213868b0fefa974947890f246c8b6d
andrewplotner <andrewplotner@yahoo.com>
2020-08-17 14:11:12 -0400

    Update step6-verify.sh

--- 16. Exercise-Storage Basics/step6-verify.sh
+++ 16. Exercise-Storage Basics/step6-verify.sh
@@ -1,1 +1,1 @@
-OUT=$( df -hP /mnt/media | grep Professor | awk '{print $2}' ) && [[ $OUT -eq "18G" ]] && echo done
+OUT=$( df -hP /mnt/media | grep Professor | awk '{print $2}' ) && [[ $OUT == 18G ]] && echo done
