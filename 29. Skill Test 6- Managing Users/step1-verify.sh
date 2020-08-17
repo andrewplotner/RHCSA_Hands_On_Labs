fe91df5386648186276e24b26b80a0aebbd70ca3
andrewplotner <andrewplotner@yahoo.com>
2020-08-17 07:24:55 -0400

    Update step1-verify.sh

--- 29. Skill Test 6- Managing Users/step1-verify.sh
+++ 29. Skill Test 6- Managing Users/step1-verify.sh
@@ -1,1 +1,1 @@
-group=$( cat /etc/group | grep Helix | cut -d: -f1 ) && [[ $group == Twitch ]] && echo "done"
+group=$( cat /etc/group | grep Helix | cut -d: -f1 ) && [[ $group == Twitch ]] && PASS=$( cat /etc/login.defs | grep PASS_WARN_AGE | awk '{print $2}' ) && [[ $PASS == 14 ]] && echo "done"
