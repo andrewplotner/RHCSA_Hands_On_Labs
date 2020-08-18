02120e04629074efb3a43f98a7882f8130117109
andrewplotner <andrewplotner@yahoo.com>
2020-08-18 13:42:07 -0400

    Update foreground.sh

--- 25. Skill Test 2- mbr,gpt,filesystems, mounting/foreground.sh
+++ 25. Skill Test 2- mbr,gpt,filesystems, mounting/foreground.sh
@@ -1,13 +1,3 @@
-#!/bin/bash
-function print_color(){ 
-case $1 in
-"green")COLOR="\033[0;32m";;
-"red")COLOR="\033[0;32m";;
-"*")COLOR="\033[0m";;
-esac
-echo -e "${COLOR} $2 ${NC}"
-}
-
 yum install -y gdisk xfsprogs
 
 clear 
